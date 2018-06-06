267a0d93093506187ba22784ced2950db8349c0a
Karthekeyan <karthekeyan.eprointern@gmail.com>
2018-06-06 11:22:55 +0800

    Added - New configuration for production env

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ let env = process.env.NODE_ENV || 'development';
 
 console.log('Environment:', env);
 
-if(env === 'development' || env === 'test') {
+if(env === 'production' || env === 'development' || env === 'test') {
 
     let config = require('./config.json');
     let envconfig = config[env];
